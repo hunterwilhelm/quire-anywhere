611e57ee88ef2fae2b9d741e04fc0aa48029400e
Hunter Wilhelm <yeskindofday@gmail.com>
2020-02-01 11:55:54 -0700

    Save access token to chrome storage

--- app/views/popup/popup.js
+++ app/views/popup/popup.js
@@ -1,2 +1,9 @@
+import {StorageService} from "../../modules/storage.service.js";
+
 document.querySelector("#url").innerHTML = "Click to log in";
 document.querySelector("#url").href = chrome.extension.getURL('/views/login/login.html#window');
+
+console.log("getting...");
+StorageService.readLocal('quire_access_token', function(result) {
+  document.querySelector('#access-token').innerHTML = result.quire_access_token;
+});
